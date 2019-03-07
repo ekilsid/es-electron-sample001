34694df1cfc14eb304399b528418c94433454c99
Takano Yasuhiro <takano.yasuhiro@ekilsid.com>
2019-03-07 22:34:54 +0700

    add gitattributes

--- app/renderer.js
+++ app/renderer.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Top from './components/top.jsx';
+import Home from './components/Home.jsx';
 
-ReactDOM.render(<Top />, document.getElementById('root'));
+ReactDOM.render(<Home />, document.getElementById('root'));
